4c308e3a08951688a32684c883ffe34469c3cdad
Sarthak Gupta <sarthakg555@gmail.com>
2017-06-07 15:59:57 +0530

    O(nlogn)

--- SetBits.java
+++ SetBits.java
@@ -1,25 +1,30 @@
 package BitAlgorithm;
 import java.util.*;
 
-//Count total number of set bits in given integer
+//Count total number of set bits from 1 to given integer
 public class SetBits {
 	static void count_setBits(int n)
 	{
-		int k = (int)(Math.log(n)/0.693)+1;
-		//System.out.println(k);
 		int count=0;
-		for(int i=0;i<k;i++)
+		for(int i=1;i<=n;i++)
 		{
-			int set = n&1<<i;
-			if(set>0)
-				count++;
+			count+=countSetBit(i);
 		}
 		System.out.println(count);
 	}
 
+	static int countSetBit(int x)
+	{
+		if(x<=0)
+		{
+			return 0;
+		}
+		
+		return (x%2==0? 0:1) + countSetBit(x/2);
+	}
 	public static void main(String args[])
 	{
-		int n = 7;
+		int n = 4;
 		
 		count_setBits(n);
 	}
