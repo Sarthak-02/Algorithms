f586934ead014e8d1b34c7efd3d701adf31034f7
Sarthak Gupta <sarthakg555@gmail.com>
2017-06-08 13:05:36 +0530

    without using Arithematic operator

---
+++ Add_two_number.java
@@ -0,0 +1,20 @@
+package BitAlgorithm;
+//To add two numbers without using Arithematic operator
+public class Add_two_numbers {
+
+	public static void main(String args[])
+	{
+		int a =10; //first number
+		int b =12; //second number
+		
+		int c = a+b;
+		
+		int xor = a^b;
+		int and = (a&b)<<1;
+		int result = xor | and;
+		System.out.println("Answer using arithmeatic operator"+c);
+		System.out.println("Answer without using arithematic operator"+result);
+		
+	}
+	
+}
